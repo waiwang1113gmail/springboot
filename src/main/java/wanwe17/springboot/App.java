824c0712272fdfd2f857995ff8a13cbdc6044cbd
wei wang <waiwang1113@gmail.com>
2016-03-15 14:41:47 -0400

    Add csrf filtering

--- src/main/java/wanwe17/springboot/App.java
+++ src/main/java/wanwe17/springboot/App.java
@@ -14,56 +14,66 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 /**
  * Hello world!
- *
+ * 
  */
 @SpringBootApplication
-public class App 
-{
-	
-    public static void main( String[] args )
-    {
-    	@SuppressWarnings("unused")
+public class App {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		ApplicationContext ctx = SpringApplication.run(App.class, args);
-    } 
-    @Configuration
+	}
+
+	@Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends
 			WebSecurityConfigurerAdapter {
-    	@Autowired
-    	DataSource dataSource;
-    	
-    	@Autowired
-    	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    		
-    	  auth.jdbcAuthentication().dataSource(dataSource)
-    		.usersByUsernameQuery(
-    			"select username,password, enabled from users where username=?")
-    		.authoritiesByUsernameQuery(
-    			"select username, role from user_roles where username=?");
-    	}	
-    	
+		@Autowired
+		DataSource dataSource;
+
+		@Autowired
+		public void configAuthentication(AuthenticationManagerBuilder auth)
+				throws Exception {
+
+			auth.jdbcAuthentication()
+					.dataSource(dataSource)
+					.usersByUsernameQuery(
+							"select username,password, enabled from users where username=?")
+					.authoritiesByUsernameQuery(
+							"select username, role from user_roles where username=?");
+		}
+
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.httpBasic()
-					.and()
-					.authorizeRequests()
-					.antMatchers("/app.html", "/home.html",
-							"/login.html", "/").permitAll();
+			http.httpBasic().and().authorizeRequests()
+					.antMatchers("/app.html", "/home.html", "/login.html", "/")
+					.permitAll().anyRequest().authenticated().and()
+					.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+					.csrf().csrfTokenRepository(csrfTokenRepository());
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
 		}
 	}
-    
-    @Bean(name = "dataSource")
+
+	@Bean(name = "dataSource")
 	public DriverManagerDataSource dataSource() {
-	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-	    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/springboot");
-	    driverManagerDataSource.setUsername("waiwang1113");
-	    driverManagerDataSource.setPassword("password");
-	    return driverManagerDataSource;
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		driverManagerDataSource
+				.setUrl("jdbc:mysql://localhost:3306/springboot");
+		driverManagerDataSource.setUsername("waiwang1113");
+		driverManagerDataSource.setPassword("password");
+		return driverManagerDataSource;
 	}
-	
-    
+
 }
