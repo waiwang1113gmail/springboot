1ee2a8609a251161c12706e301abd5db45a621df
Weige <waiwang1113@gmail.com>
2016-03-16 23:57:00 -0400

    register login changes

--- src/main/java/wanwe17/springboot/Controller.java
+++ src/main/java/wanwe17/springboot/Controller.java
@@ -5,15 +5,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,21 +41,27 @@ public class Controller {
 	@Autowired
 	JdbcTemplate tmpl;
 	
+	@Autowired
+    protected AuthenticationManager authenticationManager;
+	
 	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public ResponseEntity<String> register(@RequestBody User user) { 
-		
-		System.out.println(user.getUsername());
-		
+	public ResponseEntity<String> register(@RequestBody User user,HttpServletRequest request) { 
+		 
 		String SQL = "SELECT username FROM users WHERE username=?";
 		SqlRowSet result=tmpl.queryForRowSet(SQL,user.getUsername());
 		if(result.next()){
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("username already exists");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("\"username already exists\"");
 		}
-		String addSQL="INSERT INTO users(username,password,enabled) values (?,?,1)";
-		tmpl.update(addSQL, user.getUsername(),user.getPassword());
-		String addRole="INSERT INTO user_roles(username,role) values (?,\"ROLE_USER\")";
-		tmpl.update(addRole, user.getUsername());
+		String addSQL="CALL new_user(?,?)";
+		tmpl.update(addSQL, user.getUsername(),user.getPassword()); 
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername()	, user.getPassword());
+		request.getSession();
+		token.setDetails(new WebAuthenticationDetails(request));
+		Authentication authenticatedUser = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
 		return ResponseEntity.ok("\"pass\"");
 	}
+	
 	 
 }
